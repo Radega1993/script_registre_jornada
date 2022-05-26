0f8484ed560eea10c8f6852b2a638676e2f7a81f
raul <rauldearriba@gmail.com>
2022-05-26 08:23:28 +0200

    fix var names

--- registre_jornada/app.py
+++ registre_jornada/app.py
@@ -60,48 +60,42 @@ def final_jornada(driver):
 
 def cargar_horario(today):
 	load_dotenv()
-	inicio_jornada = os.environ.get('inicio_'+str(today))
-	inicio_comida = os.environ.get('inicioc_'+str(today))
-	final_comida = os.environ.get('finalc_'+str(today))
-	final_jornada = os.environ.get('final_'+str(today))
-	hora_test = "15:39"
-	return inicio_jornada, inicio_comida, final_comida, final_jornada, hora_test
+	inicio_jornada_hora = os.environ.get('inicio_'+str(today))
+	inicio_comida_hora = os.environ.get('inicioc_'+str(today))
+	final_comida_hora = os.environ.get('finalc_'+str(today))
+	final_jornada_hora = os.environ.get('final_'+str(today))
+	hora_test = "08:21"
+	return inicio_jornada_hora, inicio_comida_hora, final_comida_hora, final_jornada_hora, hora_test
 
 
 def main():
 
 	if (datetime.today().weekday() != 5) and (datetime.today().weekday() != 6):
 		today = datetime.today().weekday()
-		inicio_jornada, inicio_comida, final_comida, final_jornada, hora_test = cargar_horario(today)
+		inicio_jornada_hora, inicio_comida_hora, final_comida_hora, final_jornada_hora, hora_test = cargar_horario(today)
 		now = datetime.now()
 		current_time = now.strftime("%H:%M")
 
-		if str(hora_test) != "no":
-			if str(current_time) == str(hora_test):
-				logging.warning('ENTRA OK')
-				driver = webdriver.Chrome()
-				login(driver)
-
-		if str(inicio_jornada) != "no":
-			if str(current_time) == str(inicio_jornada):
+		if str(inicio_jornada_hora) != "no":
+			if str(current_time) == str(inicio_jornada_hora):
 				driver = webdriver.Chrome()
 				login(driver)
 				inicio_jornada(driver)
 
-		if str(inicio_comida) != "no":
-			if str(current_time) == str(inicio_comida):
+		if str(inicio_comida_hora) != "no":
+			if str(current_time) == str(inicio_comida_hora):
 				driver = webdriver.Chrome()
 				login(driver)
 				inicio_comida(driver)
 
-		if str(final_comida) != "no":
-			if str(current_time) == str(final_comida):
+		if str(final_comida_hora) != "no":
+			if str(current_time) == str(final_comida_hora):
 				driver = webdriver.Chrome()
 				login(driver)
 				final_comida(driver)
 
-		if str(final_jornada) != "no":
-			if str(current_time) == str(final_jornada):
+		if str(final_jornada_hora) != "no":
+			if str(current_time) == str(final_jornada_hora):
 				driver = webdriver.Chrome()
 				login(driver)
 				final_jornada(driver)
